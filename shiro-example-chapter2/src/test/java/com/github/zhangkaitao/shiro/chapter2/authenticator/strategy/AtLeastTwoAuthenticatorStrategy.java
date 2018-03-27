3b6edfd78392d6fa56a6e42c1c86b86e5434f8b9
sunpanhu <yUhU3552247687>
2018-03-27 16:36:15 +0800

    添加注释

--- shiro-example-chapter2/src/test/java/com/github/zhangkaitao/shiro/chapter2/authenticator/strategy/AtLeastTwoAuthenticatorStrategy.java
+++ shiro-example-chapter2/src/test/java/com/github/zhangkaitao/shiro/chapter2/authenticator/strategy/AtLeastTwoAuthenticatorStrategy.java
@@ -5,29 +5,32 @@ import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.pam.AbstractAuthenticationStrategy;
-import org.apache.shiro.authc.pam.AuthenticationStrategy;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.util.CollectionUtils;
 
 import java.util.Collection;
 
 /**
+ * 自定义AuthenticationStrategy
  * <p>User: Zhang Kaitao
  * <p>Date: 14-1-25
  * <p>Version: 1.0
  */
 public class AtLeastTwoAuthenticatorStrategy extends AbstractAuthenticationStrategy {
 
+    //在所有Realm验证之前调用
     @Override
     public AuthenticationInfo beforeAllAttempts(Collection<? extends Realm> realms, AuthenticationToken token) throws AuthenticationException {
         return new SimpleAuthenticationInfo();//返回一个权限的认证信息
     }
 
+    //在每个Realm之前调用
     @Override
     public AuthenticationInfo beforeAttempt(Realm realm, AuthenticationToken token, AuthenticationInfo aggregate) throws AuthenticationException {
         return aggregate;//返回之前合并的
     }
 
+    //在每个Realm之后调用
     @Override
     public AuthenticationInfo afterAttempt(Realm realm, AuthenticationToken token, AuthenticationInfo singleRealmInfo, AuthenticationInfo aggregateInfo, Throwable t) throws AuthenticationException {
         AuthenticationInfo info;
@@ -44,6 +47,7 @@ public class AtLeastTwoAuthenticatorStrategy extends AbstractAuthenticationStrat
         return info;
     }
 
+    //在所有Realm之后调用
     @Override
     public AuthenticationInfo afterAllAttempts(AuthenticationToken token, AuthenticationInfo aggregate) throws AuthenticationException {
         if (aggregate == null || CollectionUtils.isEmpty(aggregate.getPrincipals()) || aggregate.getPrincipals().getRealmNames().size() < 2) {
